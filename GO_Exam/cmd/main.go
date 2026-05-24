7196c8c0a0947105ee6a77c725ec5606754f7000
Aurik <Aurikgg@gmail.com>
2026-05-24 23:11:26 +0500

    deploy

--- GO_Exam/cmd/main.go
+++ GO_Exam/cmd/main.go
@@ -4,6 +4,7 @@ package main
 import (
 	"github.com/gin-contrib/cors"
 	"log"
+	"os"
 
 	"project/itStep/internal/config"
 	"project/itStep/internal/routes"
@@ -17,18 +18,22 @@ import (
 func main() {
 	
 	
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("env not loaded")
-	}
+err := godotenv.Load()
+if err != nil {
+    log.Println(".env not found")
+}
 
 	config.ConnectDB()
 
 	r := gin.Default()
+	r.SetTrustedProxies(nil)
 
 	
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowOrigins: []string{
+			"http://localhost:5173",
+			"https://it-step-social-hfxlennyy-auriks-projects.vercel.app",
+		},
 		AllowMethods:     []string{"GET", "POST", "DELETE", "PUT"},
 		AllowHeaders:     []string{"Authorization", "Content-Type"},
 		AllowCredentials: true,
@@ -39,5 +44,10 @@ func main() {
 	
 	r.Static("/uploads", "./uploads")
 	go handler.HandleMessages()
-	r.Run(":8080")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	r.Run("0.0.0.0:" + port)
 }
